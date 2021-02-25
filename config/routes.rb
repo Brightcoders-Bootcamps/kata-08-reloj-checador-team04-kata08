545347a9a3d6d212e6ee0e1fe7d6c555b78a236a
OchoaHub <jochoa2@ucol.com>
2021-02-25 17:03:14 -0600

    Modify | Heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  #root 'sessions#table'
+  root 'sessions#login'
 
   get 'sessions/login'
-  post 'sessions/login' => 'sessions#create'
+  post '/' => 'sessions#create'
 
   get 'sessions/show'
 
